bda8e4711d9bd9fe4f8a7e9d666e35df6e5b80ba
bnguyen-nanolos <86146840+thienpham1567@users.noreply.github.com>
2023-06-12 23:11:35 +0700

    Update UI navbar

--- src/components/Layout/ATHeader.vue
+++ src/components/Layout/ATHeader.vue
@@ -1,22 +1,39 @@
 <template>
-    <div class="flex items-center justify-between px-4">
-        <div class="flex items-center">
-            <Image :src="Logo" alt="Image" width="170" />
-            <div class="p-inputgroup w-96">
-                <InputText placeholder="Search for shoes, clothes, etc." />
-                <Button icon="pi pi-search" severity="success" />
+    <nav>
+        <div class="flex items-center justify-between px-4">
+            <div class="flex items-center">
+                <Image :src="Logo" alt="Image" width="250" />
+                <div class="p-inputgroup w-96">
+                    <AutoComplete v-model="selectedProduct" optionLabel="name" :suggestions="filteredProducts"
+                        @complete="search" placeholder="Search for shoes, clothes, etc.">
+                        <!-- <template #option="slotProps">
+                            <div class="flex align-options-center">
+                                <img :alt="slotProps.option.name"
+                                    src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png"
+                                    :class="`flag flag-${slotProps.option.code.toLowerCase()} mr-2`" style="width: 18px" />
+                                <div>{{ slotProps.option.name }}</div>
+                            </div>
+                        </template> -->
+                    </AutoComplete>
+                    <Button icon="pi pi-search" class="search-btn" />
+                </div>
             </div>
+            <Button icon="pi pi-shopping-cart" class="cart-btn" label="MY CART" raised />
         </div>
-        <div>
-            <Button icon="pi pi-shopping-cart" label="MY CART" severity="success" outlined />
-        </div>
-    </div>
+    </nav>
 </template>
 
 <script setup lang="ts">
 import Image from 'primevue/image';
 import Button from 'primevue/button';
-import InputText from 'primevue/InputText';
+import AutoComplete from 'primevue/autocomplete';
 import Logo from "@/assets/images/logo.png"
+import { ref } from 'vue';
 
+const selectedProduct = ref();
+const filteredProducts = ref([]);
+
+function search() {
+
+}
 </script>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
 import ATHeader from '@/components/Layout/ATHeader.vue';
 import ATFooter from '@/components/Layout/ATFooter.vue';
-// import Login from '@/views/Login/Login.vue';
-// import Registration from '@/views/Registration/Registration.vue';
 </script>
 
 <template>
-    <!-- <Login />
-        <Registration /> -->
-    <ATFooter />
+    <div class="w-screen h-screen">
+        <header>
+            <ATHeader/>
+        </header>
+        <main>
+            <router-view />
+        </main>
+        <footer>
+            <ATFooter />
+        </footer>
+    </div>
 </template>
