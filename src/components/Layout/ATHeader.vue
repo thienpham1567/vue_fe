5fbe75a0603ec6adf7847b356ea7468ebf41e1df
nguyenduyan0411 <nguyenduyan0411@gmail.com>
2023-07-02 10:04:22 +0700

    update

--- src/components/Layout/ATHeader.vue
+++ src/components/Layout/ATHeader.vue
@@ -59,34 +59,7 @@
     <template #header>
       <div class="text-2xl">Added To Cart</div>
     </template>
-    <div class="overflow-y-auto w-full h-5/6">
-      <div class="mb-1 flex flex-wrap">
-        <div class="rounded-lg flex w-full ml-4 mr-4">
-          <div class="w-1/4 ">
-            <img src="https://m.media-amazon.com/images/I/71qvDARKU3L._AC_SR1840,1472_.jpg"
-              class="h-full w-full object-contain" />
-          </div>
-          <div class="w-2/4 ml-4">
-            <div class="text-base font-light">Rebok Work</div>
-            <div class="text-xl font-bold">Adidas</div>
-            <div class="text-base font-light">Color: black</div>
-            <div class="text-base font-light">Size: Men 12</div>
-          </div>
-          <div class="w-1/4">
-            <div class="text-xl font-bold">$114</div>
-            <div class="flex justify-center mt-2">
-              <Button icon="pi pi-minus" class="p-button-secondary p-button-text mr-2"
-                @click="decrement(products)"></Button>
-              <div class="bg-white w-12 flex justify-center items-center">{{ products[0].quantity }}</div>
-              <Button icon="pi pi-plus" class="p-button-secondary p-button-text ml-2"
-                @click="increment(products)"></Button>
-
-            </div>
-            <div class="mt-2">Remove</div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <CartItem />
     <div class="bg-gray-200 w-full h-1/6">
       <div class="flex justify-end mr-4">Cart Subtotal (10 Items)$1,099.90</div>
       <div class="flex justify-between m-4 pb-4">
@@ -116,10 +89,10 @@ import MegaMenu from 'primevue/megamenu';
 import Logo from "@/assets/images/logo.png";
 import CoreDialog from '@/components/Core/CoreDialog.vue';
 import Sidebar from 'primevue/sidebar';
-import { ref } from 'vue';
+import CartItem from "@/components/CartItems/CartItem.vue";
 import { useRouter } from 'vue-router';
 import { useBrandStore, useAccountStore, useCategoryStore } from "@/store";
-import { onMounted, onUpdated } from 'vue';
+import { onMounted, onUpdated, ref } from 'vue';
 import jwt_decode from "jwt-decode";
 
 enum MainCategories {
